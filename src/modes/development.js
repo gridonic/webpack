5fa2265aa47a12afda65bef68b77e7ce449b003c
Sebastian Prein <hi@sebastianprein.com>
2019-01-18 11:30:56 +0100

    feat: add hot module replacement

--- src/modes/development.js
+++ src/modes/development.js
@@ -1,5 +1,6 @@
 const entry = require('../entry');
 const output = require('../output');
+const hmr = require('../hmr');
 
 // @see https://webpack.js.org/guides/development/
 module.exports = {
@@ -16,8 +17,18 @@ module.exports = {
     devServer: {
 
         // @see https://webpack.js.org/configuration/dev-server/#devserver-contentbase
-        contentBase: output.path
+        contentBase: output.path,
+
+        // @see https://webpack.js.org/configuration/dev-server/#devserver-hot
+        // @see https://webpack.js.org/concepts/hot-module-replacement/
+        // @see https://webpack.js.org/guides/hot-module-replacement/
+        hot: true
     },
+
+    // @see https://webpack.js.org/configuration/plugins/
+    plugins: [
+        hmr()
+    ],
 
     // @see https://webpack.js.org/configuration/output/
     output: Object.assign({}, output, {
