bc76b1b127402743babbdcb1528cbb40a18dd662
Sebastian Prein <hi@sebastianprein.com>
2019-01-21 15:44:06 +0100

    docs: add link to hmr docs

--- src/plugins/hmr.js
+++ src/plugins/hmr.js
@@ -1,3 +1,4 @@
 const webpack = require('webpack');
 
+// @see https://webpack.js.org/plugins/hot-module-replacement-plugin/
 module.exports = (options = {}) => new webpack.HotModuleReplacementPlugin(options);
