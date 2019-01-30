14997f671000b30df51db45bd5c99c6062ee969d
Sebastian Prein <hi@sebastianprein.com>
2019-01-30 12:02:31 +0100

    refactor: remove defaults from raw preset

--- src/presets/raw.js
+++ src/presets/raw.js
@@ -1,13 +1,7 @@
-const delve = require('dlv');
-
-const defaults = {
-    test: /\.txt$/
-};
-
 module.exports = (options = {}) => ({
     module: {
         rules: [{
-            test: delve(options, 'test', defaults.test),
+            test: options.test,
 
             // @see https://github.com/webpack-contrib/raw-loader
             use: 'raw-loader'
