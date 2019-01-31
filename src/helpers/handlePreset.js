14f1858127f2bccb349c4363428342909e2351ae
Sebastian Prein <hi@sebastianprein.com>
2019-01-31 22:29:18 +0100

    refactor: options.preset should not contain functions

--- src/helpers/handlePreset.js
+++ src/helpers/handlePreset.js
@@ -1,19 +1,16 @@
 const merge = require('webpack-merge');
 
 module.exports = (preset, defaults = {}) => {
+    let fn;
+    let options;
+
     if (Array.isArray(preset) === true) {
-        const [fn, options] = preset;
-
-        return fn(merge(defaults, options));
+        [fn, options] = preset;
+    } else if (typeof preset === 'string') {
+        fn = preset;
+    } else {
+        throw new Error('Preset must be of type array or string.');
     }
 
-    if (typeof preset === 'function') {
-        return preset(defaults);
-    }
-
-    if (typeof preset === 'string') {
-        return require(`../presets/${preset}.js`)(defaults);
-    }
-
-    throw new Error('Preset must be of type array, function or string.');
+    return require(`../presets/${fn}.js`)(merge(defaults, options));
 };
