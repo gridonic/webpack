437848a6b737cb6b55efcfb2bf36a66dd933be41
Sebastian Prein <hi@sebastianprein.com>
2019-02-04 14:43:31 +0100

    fix: vue preset installing dependencies without ever being used

--- src/presets/vue.js
+++ src/presets/vue.js
@@ -12,52 +12,55 @@ const defaults = {
     test: /\.vue$/
 };
 
-// Require packages that are used by this preset
-localRequire({
-    'vue': '^2.5.22',
-    'vue-i18n': '^8.8.0',
-    'vue-router': '^3.0.2',
-    'vuex': '^3.1.0'
-});
+module.exports = (options = {}) => {
 
-localRequire({
-    'vue-template-compiler': '^2.5.22'
-}, { dev: true });
+    // Require packages that are used by this preset
+    localRequire({
+        'vue': '^2.5.22',
+        'vue-i18n': '^8.8.0',
+        'vue-router': '^3.0.2',
+        'vuex': '^3.1.0'
+    });
 
-module.exports = (options = {}) => merge({
-    module: {
-        rules: [{
-            test: delve(options, 'test', defaults.test),
+    localRequire({
+        'vue-template-compiler': '^2.5.22'
+    }, { dev: true });
 
-            // @see https://github.com/vuejs/vue-loader
-            use: {
-                loader: 'vue-loader',
-                options: {
-                    compiler: importCwd('vue-template-compiler')
+    return merge({
+        module: {
+            rules: [{
+                test: delve(options, 'test', defaults.test),
+
+                // @see https://github.com/vuejs/vue-loader
+                use: {
+                    loader: 'vue-loader',
+                    options: {
+                        compiler: importCwd('vue-template-compiler')
+                    }
                 }
-            }
-        }, {
+            }, {
 
-            // @see https://kazupon.github.io/vue-i18n/guide/sfc.html
-            resourceQuery: /blockType=i18n/,
-            type: 'javascript/auto',
-            use: [
-                '@kazupon/vue-i18n-loader',
-                'yaml-loader'
-            ]
-        }]
-    },
-    plugins: [
-        new VueLoaderPlugin({
-            productionMode: options.mode === 'production'
-        })
-    ]
-}, eslint(
+                // @see https://kazupon.github.io/vue-i18n/guide/sfc.html
+                resourceQuery: /blockType=i18n/,
+                type: 'javascript/auto',
+                use: [
+                    '@kazupon/vue-i18n-loader',
+                    'yaml-loader'
+                ]
+            }]
+        },
+        plugins: [
+            new VueLoaderPlugin({
+                productionMode: options.mode === 'production'
+            })
+        ]
+    }, eslint(
 
-    // @todo Why is vue-loader not picking this from our js preset?
-    // @see https://vue-loader.vuejs.org/guide/#manual-configuration
-    merge({
-        mode: options.mode,
-        test: delve(options, 'test', defaults.test)
-    }, options.eslint)
-));
+        // @todo Why is vue-loader not picking this from our js preset?
+        // @see https://vue-loader.vuejs.org/guide/#manual-configuration
+        merge({
+            mode: options.mode,
+            test: delve(options, 'test', defaults.test)
+        }, options.eslint)
+    ));
+};
