143fda4e41e0dfec41aa8ee1f484229cb64b0bc7
Sebastian Prein <hi@sebastianprein.com>
2019-02-04 09:37:37 +0100

    feat: add cli

---
+++ bin/cli.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+const meow = require('meow');
+const shell = require('shelljs');
+const chalk = require('chalk');
+
+const logo = require('./logo');
+const pkg = require('../package.json');
+
+const cli = meow(`
+${logo}
+${chalk.bold.blue(pkg.name)} ${chalk.gray(`(v${pkg.version})`)}
+	
+
+	Usage
+	  $ gridonic-webpack
+
+	Options
+	  --production, -p  Run in production mode
+`, {
+    description: false,
+    flags: {
+        production: {
+            type: 'boolean',
+            alias: 'p',
+            default: false
+        }
+    }
+});
+
+const { production } = cli.flags;
+
+console.log(`
+${logo}
+${chalk.bold.blue(pkg.name)} ${chalk.gray(`(v${pkg.version})`)}
+
+Running in ${chalk.green(production === true ? 'production' : 'development')} mode…
+`);
+
+// Run in production mode
+if (production === true) {
+    return shell.exec('webpack --env production');
+}
+
+// Run in development mode…
+shell.exec('webpack-dev-server');
