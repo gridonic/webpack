d27ed852d451aa777060af7c8b13486985dc03e8
Sebastian Prein <hi@sebastianprein.com>
2019-02-08 13:32:01 +0100

    docs: explain why we only serve woff/woff2

--- src/presets/fonts.js
+++ src/presets/fonts.js
@@ -1,7 +1,14 @@
 const delve = require('dlv');
 
 const defaults = {
+
+    // We are only serving woff and woff2 font files since they are just
+    // superior to legacy formats (ttf, otf, eot) and the support for these
+    // formats is pretty decent by now.
+    //
+    // @see https://caniuse.com/#search=woff
     test: /\.woff2?$/,
+
 };
 
 module.exports = (options = {}) => ({
