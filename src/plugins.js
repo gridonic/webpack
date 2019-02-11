15a225a0f4026336693fa8839951f04b86c41598
Sebastian Prein <hi@sebastianprein.com>
2019-02-11 13:51:09 +0100

    refactor: drop write-file-webpack-plugin
    It’s already supported by webpack-dev-middleware.
    
    See https://github.com/webpack/webpack-dev-middleware#writetodisk

--- src/plugins.js
+++ src/plugins.js
@@ -2,12 +2,10 @@ const clean = require('./plugins/clean');
 const friendlyErrors = require('./plugins/friendlyErrors');
 const html = require('./plugins/html');
 const miniCssExtract = require('./plugins/miniCssExtract');
-const writeFile = require('./plugins/writeFile');
 
 module.exports = {
     clean,
     friendlyErrors,
     html,
-    miniCssExtract,
-    writeFile
+    miniCssExtract
 };
--- src/plugins/writeFile.js
+++
@@ -1,4 +0,0 @@
-// @see https://github.com/gajus/write-file-webpack-plugin
-const WriteFilePlugin = require('write-file-webpack-plugin');
-
-module.exports = (options = {}) => new WriteFilePlugin(options);
