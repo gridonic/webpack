d49f105459eb9af2ae1c6132aed33a5cf7447f50
Sebastian Prein <hi@sebastianprein.com>
2019-02-17 23:19:05 +0100

    feat(dev-server): make it quiet

--- src/options/devServer.js
+++ src/options/devServer.js
@@ -1,6 +1,5 @@
 const merge = require('webpack-merge');
 
-const stats = require('./stats');
 const context = require('./context');
 
 // @see https://webpack.js.org/configuration/dev-server/
@@ -12,14 +11,25 @@ const defaults = {
     // @see https://webpack.js.org/configuration/dev-server/#devserver-open
     open: true,
 
-    // @see https://webpack.js.org/configuration/stats/
-    stats,
+    // @see https://webpack.js.org/configuration/dev-server#devserver-noinfo
+    noInfo: true,
+
+    // @see https://webpack.js.org/configuration/dev-server#devserver-quiet
+    quiet: true,
 
     // @see https://webpack.js.org/configuration/dev-server/#devserver-overlay
     overlay: true,
 
     // @see https://webpack.js.org/configuration/dev-server/#devserver-host,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+
+    // @see https://webpack.js.org/configuration/dev-server/#devserver-port
+    port: 8080,
+
+    // @see https://webpack.js.org/configuration/dev-server/#devserver-hot
+    // @see https://webpack.js.org/concepts/hot-module-replacement/
+    // @see https://webpack.js.org/guides/hot-module-replacement/
+    hot: true
 };
 
 module.exports = (options = {}) => merge(defaults, options);
