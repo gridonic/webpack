2d974929f54c081ac5053d83a1075e6908242d31
Sebastian Prein <hi@sebastianprein.com>
2019-02-18 21:31:25 +0100

    refactor: remove cli.run

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,11 @@
 const { name, description, version } = require('../package.json');
 const commands = require('./cli/commands');
 const flags = require('./cli/flags');
-const run = require('./cli/run');
 
 module.exports = {
     name,
     description,
     version,
-    run,
     flags,
     commands,
 };
--- src/cli/run.js
+++
@@ -1,7 +0,0 @@
-const commands = require('./commands');
-
-module.exports = (command, args) => {
-    const { fn } = commands.find(item => item.command === command);
-
-    fn(args);
-};
