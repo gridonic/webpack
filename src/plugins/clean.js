07d64706ad4081a91e7708845e6fa3d1740df769
Sebastian Prein <hi@sebastianprein.com>
2019-03-20 13:39:59 +0100

    chore: adjust clean plugin to 2.x release

--- src/plugins/clean.js
+++ src/plugins/clean.js
@@ -1,35 +1,17 @@
 const merge = require('webpack-merge');
 
-const output = require('../options/output');
-const context = require('../options/context');
-
-// @see https://github.com/developit/dlv
-const delve = require('dlv');
-
 // @see https://github.com/johnagan/clean-webpack-plugin
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-// @see https://github.com/scottcorgan/as-array
-const asArray = require('as-array');
-
-// @see https://github.com/martinandert/except
-const except = require('except');
-
 const defaults = {
-    path: output().path,
-    options: {
-        root: context(),
-        verbose: false
-    }
+    verbose: false
 };
 
 module.exports = (options = {}) => {
-    const path = asArray(
-        delve(options, 'path', defaults.path)
+    return new CleanWebpackPlugin(
+        merge(
+            defaults.options,
+            options
+        )
     );
-
-    return new CleanWebpackPlugin(path, merge(
-        defaults.options,
-        except(options, 'path')
-    ));
 };
