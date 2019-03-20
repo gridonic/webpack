ba0f4560642d6251c89ae82f45fe333223522ebd
Sebastian Prein <hi@sebastianprein.com>
2019-03-20 15:22:15 +0100

    fix: env helper throwing error if env is not found

--- src/helpers/env.js
+++ src/helpers/env.js
@@ -1,16 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
 // @see https://github.com/developit/dlv
 const delve = require('dlv');
 
-// Reads the variables from .env files using dotenv.config() so they can be
-// accessed by process.env.
-//
-// @see https://github.com/motdotla/dotenv
-const result = require('dotenv').config();
-
-if (result.error) {
-    throw result.error;
-}
-
 module.exports = (name, defaultValue) => {
+    if (fs.existsSync(path.resolve(process.cwd(), '.env')) === false) {
+        return defaultValue;
+    }
+
+    // Reads the variables from .env files using dotenv.config() so they can be
+    // accessed by process.env.
+    //
+    // @see https://github.com/motdotla/dotenv
+    const result = require('dotenv').config();
+
+    if (result.error) {
+        throw result.error;
+    }
+
     return delve(process, `env.${name}`, defaultValue);
 };
