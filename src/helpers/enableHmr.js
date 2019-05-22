b9f5226a78fe3f5c1be4d561436d5a670492cd57
Sebastian Prein <hi@sebastianprein.com>
2019-05-22 15:56:37 +0200

    fix(hmr): using wrong socketjs port when auto detecting

--- src/helpers/enableHmr.js
+++ src/helpers/enableHmr.js
@@ -1,6 +1,8 @@
 // @see https://github.com/webpack/webpack
 const webpack = require('webpack');
 
+const setIfUnknown = require('../helpers/setIfUnknown');
+
 module.exports = ({ protocol, host, port, webpackConfig }) => {
     const hotEntries = [
         `webpack-dev-server/client?${protocol}://${host}:${port}/`,
@@ -9,6 +11,11 @@ module.exports = ({ protocol, host, port, webpackConfig }) => {
 
     // @see https://webpack.js.org/plugins/hot-module-replacement-plugin/
     webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+    // Make sure socketjs uses correct port. Usually webpack figures this out on it’s own,
+    // but when we run webpack with a vhost it kind of struggles.
+    // @see https://github.com/webpack/webpack-dev-server/pull/1792
+    setIfUnknown(webpackConfig, 'devServer.sockPort', port);
 
     // If webpack entry is a string,
     // convert it into an array.
